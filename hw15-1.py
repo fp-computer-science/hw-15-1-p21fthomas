b866ac0d3ebe48cfef414de36b5f3f2d62e7e4ee
p21fthomas <p21fthomas@fairfieldprep.org>
2021-02-07 20:21:14 -0500

    hw15-1.py

---
+++ hw15-1.py
@@ -0,0 +1,31 @@
+
+# Question 1
+fruits = ('banana', 'orange', 'grape', 'peach')
+
+#question 2
+print(fruits[2])
+
+#question 3
+print(fruits[1][:1])
+
+#question 4
+print(len(fruits))
+
+#question 5
+print("apple" not in fruits)
+
+#question 6
+fruits = list(fruits)
+
+#question 7
+print 
+
+#question 8
+yes = ("a") * 5
+
+#question 9 
+
+
+#question 10
+del fruits
+print(fruits)
